abd07a0475a30c2ec47668d5ed7090bee6923603
Gabriela Sato <gabisato.ssb@gmail.com>
2025-05-25 19:34:08 -0300

    chore: adds transactions routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,4 +2,7 @@ class AppRoutes {
   static const String login = '/login';
   static const String signup = '/signup';
   static const String home = '/home';
+  static const String transactions = '/transactions';
+  static const String newTransaction = '/new-transaction';
+  static const String updateTransaction = '/update-transaction';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:tech_challenge_3/service_locator.dart';
+
+import 'package:tech_challenge_3/core/configs/theme/app_theme.dart';
 import 'package:tech_challenge_3/core/routes/app_routes.dart';
+import 'package:tech_challenge_3/service_locator.dart';
+
+import 'package:tech_challenge_3/presentation/transaction/screens/update_transaction.dart';
+import 'package:tech_challenge_3/presentation/transaction/screens/new_transaction.dart';
+import 'package:tech_challenge_3/presentation/transaction/screens/transactions.dart';
 import 'package:tech_challenge_3/presentation/auth/pages/custom_signin.dart';
 import 'package:tech_challenge_3/presentation/auth/pages/custom_signup.dart';
 import 'package:tech_challenge_3/presentation/home/pages/home.dart';
@@ -25,11 +31,15 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.appTheme,
       initialRoute: AppRoutes.login,
       routes: {
         AppRoutes.login: (context) => CustomSigninPage(),
         AppRoutes.signup: (context) => CustomSignupPage(),
         AppRoutes.home: (context) => const HomePage(),
+        AppRoutes.transactions: (context) => const TransactionsScreen(),
+        AppRoutes.newTransaction: (context) => const NewTransactionScreen(),
+        AppRoutes.updateTransaction: (context) => const UpdateTransaction(),
       },
     );
   }
